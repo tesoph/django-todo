bc38630f5ab3ee9da6af2cf6efaac1d010cdf7ee
ray <12276029+tesoph@users.noreply.github.com>
2020-08-11 18:54:20 +0100

    Add allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +26,7 @@ SECRET_KEY = '%nv@#e&l8z9qzgx69j)vm=^(&eskr*_!salqp=j(c(o#j!h&x@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-todo-xxx.herokuapp.com/']
 
 
 # Application definition
